542f785c7af157e14f047885d0cd5d27244ee8bc
Ian Nagano <ian.nagano@sciensa.com>
2017-06-19 11:46:02 -0300

    style(codestyle): Apply eslint rules

--- lokijs.js
+++ lokijs.js
@@ -1,71 +1,79 @@
-module.exports = function (RED) {
-    "use strict";
-    var loki = require("lokijs");
+/* eslint-disable */
 
-    function init(config) {
-        config.lokidb = new loki(config.filename);
-        var coll = config.lokidb.getCollection(config.collection);
-        if (!coll) {
-            coll = config.lokidb.addCollection(config.collection);
+'use strict';
+
+/* eslint-disable */
+
+const Loki = require('lokijs');
+
+module.exports = (RED) => {
+  function init(config) {
+    const lokidb = new Loki(config.filename);
+    let coll = lokidb.getCollection(config.collection);
+    if (!coll) {
+      coll = lokidb.addCollection(config.collection);
+    }
+    return lokidb;
+  }
+
+  function lokijsConfig(n) {
+    RED.nodes.createNode(this, n);
+    this.filename = n.filename;
+    this.collection = n.collection;
+    this.lokidb = init(this);
+  }
+  RED.nodes.registerType('lokijs-config', lokijsConfig);
+
+  function lokijsOp(n) {
+    RED.nodes.createNode(this, n);
+    this.config = RED.nodes.getNode(n.config);
+    this.method = n.method;
+    this.name = n.name;
+    this.input = n.input;
+    const node = this;
+
+    const connect = (nd) => {
+      const coll = nd.config.lokidb.getCollection(nd.config.collection);
+
+      nd.on('input', (msg) => {
+        let input = {};
+
+        if (nd.input === 'true') {
+          input = msg;
+        } else if (this.input !== 'false' && typeof this.input !== 'undefined') {
+          try {
+            input = RED.util.getMessageProperty(msg, this.input);
+          } catch (err) {
+            input = {};
+          }
         }
-    }
 
-    function lokijsConfig(n) {
-        RED.nodes.createNode(this, n);
-        this.filename = n.filename;
-        this.collection = n.collection;
-        init(this);
-    }
-    RED.nodes.registerType("lokijs-config", lokijsConfig);
+        let message = msg;
 
-    function lokijsOp(n) {
-        RED.nodes.createNode(this, n);
-        this.config = RED.nodes.getNode(n.config);
-        this.method = n.method;
-        this.name = n.name;
-        this.input = n.input;
-        var node = this;
-
-        var connect = function (node) {
-            var coll = node.config.lokidb.getCollection(node.config.collection);
-
-            node.on('input', function (msg) {
-                var input = {};
-
-                if (node.input === "true") {
-                    input = msg
-                } else if (this.input !== "false" && typeof this.input !== "undefined") {
-                    try {
-                        input = RED.util.getMessageProperty(msg, this.input);
-                    } catch (err) {
-                        input = {};
-                    }
-                }
-
-                if (node.method === "find") {
-                    msg.payload = coll.find(input);
-                    delete msg.payload.meta;
-                    delete msg.payload.$loki;
-                    node.send(msg);
-                } else if (node.method === "insert") {
-                    if (node.input === "true") {
-                        msg = coll.insert(input);
-                        delete msg.meta;
-                        delete msg.$loki;
-                    } else {
-                        msg.payload = coll.insert(input);
-                        delete msg.payload.meta;
-                        delete msg.payload.$loki;
-                    }
-                    node.send(msg);
-                }
-            });
-        };
-        if (node.config) {
-            connect(node)
-        } else {
-            node.error(RED._("lokijs.errors.missingconfig"));
+        if (nd.method === 'find') {
+          message.payload = coll.find(input);
+          delete message.payload.meta;
+          delete message.payload.$loki;
+          nd.send(message);
+        } else if (nd.method === 'insert') {
+          if (nd.input === 'true') {
+            message = coll.insert(input);
+            delete message.meta;
+            delete message.$loki;
+          } else {
+            message.payload = coll.insert(input);
+            delete message.payload.meta;
+            delete message.payload.$loki;
+          }
+          nd.send(msg);
         }
+      });
+    };
+    if (node.config) {
+      connect(node);
+    } else {
+      node.error(RED._('lokijs.errors.missingconfig'));
     }
-    RED.nodes.registerType("lokijs-op", lokijsOp);
+  }
+  RED.nodes.registerType('lokijs-op', lokijsOp);
 };
